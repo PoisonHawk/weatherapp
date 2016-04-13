b685baaf79dc838dad707a21a3294e8c4baad99e
Yevgeny S <savochkin2010@yandex.ru>
2016-04-13 10:58:59 +0300

    switch temperature from celsius to fahrenheit and back

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ var app = {
 		'Clear': 'wi-day-sunny',
 		'Snow': 'wi-snow',
 	},
+	temp: 'celsius',
+	tempKelvin: [],
+
 	
 	setError: function(text){
 		$('.error').text(text).show();	
@@ -32,7 +35,34 @@ var app = {
 		$('#time').text(h+':'+m);
 		setTimeout(app.setTime,1000);
 	},
+
+	changeTemp: function(){
+
+		$('.temp').map(function(i, el){
+				var t = $(this).text();
+
+				if (app.temp === 'celsius') {
+					t = 1.8*(app.tempKelvin[i] - 273)+32;
+				} else {
+					t = app.tempKelvin[i] - 273.15; 
+				}
+				
+				$(this).text(Math.floor(t))
+				
+				
+			})
+
+		$('#FC i').toggleClass('wi-fahrenheit wi-celsius');
+					
+		app.temp = (app.temp === 'celsius') ? 'fahrenheit' : 'celsius';
+		
+	},
 	
+	fromKelvinToCelsius: function(temp){
+
+		return Math.floor(temp - 273.15);
+	},
+
 	getWeatherData: function(){
 	
 	var targetUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat='+app.latitude+'&lon='+app.longitude+'&APPID='+app.key+'&callback=?';
@@ -46,7 +76,7 @@ var app = {
 				
 				var i=0;
 				data.list.forEach(function(d){
-					console.log(d);
+					// console.log(d);
 					var cdate = new Date(d.dt*1000);
 									
 					$('#day_'+i+' .day').text(cdate.getDate());
@@ -58,11 +88,17 @@ var app = {
 					}
 						$('#day_'+i+' .day_of_week').text(dayOfWeek);
 					
-					$('#day_'+i+' .temp').text(Math.floor(d.temp.day));				
+					app.tempKelvin.push(d.temp.day);
+
+					$('#day_'+i+' .temp').text(app.fromKelvinToCelsius(d.temp.day));				
 					
 					$('#day_'+i+' .weather').html('<i class="wi '+app.weatherIcons[d.weather[0].main]+'"></i>');	
 					i++;
 				})
+				$('#FC').click(function(){
+						app.changeTemp();
+					});
+				console.log(app.tempKelvin);
 				
 			})
 			
